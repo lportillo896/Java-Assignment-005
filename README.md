b45994d32df582631f75305ab00e34b98703f99e
lportillo896 <lportillo896@mycr.redwoods.edu>
2023-02-26 13:24:00 -0800

    Completed Java Assignment 005

--- README.md
+++ README.md
@@ -7,17 +7,28 @@ As a new Java developer, you will be required to familiarize yourself with exist
     * Variables
 * Using Java terminology, break down every piece of the method **convertF2K**.
     * What is the access modifier?
+      * The access modifier is public
     * Is it a class or object method, how do you know?
+      * It is a class method.  It is static.
     * What is its return type?
+      * Its return type is double.
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+      * The parameter is Fahrenheit and its datatype is double.
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+      * You call the method from the main method and enter your temperature in Fahrenheit with a double datatype.  It then calls the convertC2K method with the convertF2C method as its parameter which has Fahrenheit as its parameter and double as its datatype. Then it returns convertC2K to the main method.
 * Using Java terminology, break down method **getUnitChoice**.
     * What is the access modifier?
+      * The access modifier is public.
     * Is it a class or object method, how do you know?
+      * It is a class method.  It is static.
     * What is its return type?
+      * Its return type is String.
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+      * It does not require parameters.
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+      * All four options will be printed for the user.  The user will then type their input to be passed to the scanner class.
     * How do the format specifiers differ than what you've seen before and what do they do?
+      * %-4s makes a 4 space wide space before the string.  %40s makes a 40 space wide space after the string.  %n places a new line.
 
 ## Part 2: - Implement Missing Method: convertK2F
 This Part is aimed at teaching you to use methods and code you have to add new functionality to existing code. In this case you will add the missing conversion method, but you don't need to know math :-)
--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,26 +1,40 @@
+/**
+ *
+ * @Author Trevor Hartman
+ * @Author Logan Portillo
+ *
+ * @Since Version 1.0
+ *
+ */
+
 import java.util.Scanner;
+import static java.lang.System.exit;
 
 public class TempConversion {
     static final Scanner input = new Scanner(System.in);
 
-    public static double convertC2F(double celsius) {
+    public static double convertC2F(double celsius) { //Converts Celsius to Fahrenheit
         return (celsius * (9.0/5)) + 32.0;
     }
 
-    public static double convertC2K(double celsius) {
+    public static double convertC2K(double celsius) { //Converts Celsius to Kelvin
         return celsius + 273.15;
     }
 
-    public static double convertF2C(double fahrenheit) {
+    public static double convertF2C(double fahrenheit) { //Converts Fahrenheit to Celsius
         return 5.0 * (fahrenheit - 32) / 9.0;
     }
 
-    public static double convertF2K(double fahrenheit) {
+    public static double convertF2K(double fahrenheit) { //Converts Fahrenheit to Kelvin
         return convertC2K(convertF2C(fahrenheit));
     }
 
-    public static double convertK2C(double kelvin) {
+    public static double convertK2C(double kelvin) { //Converts Kelvin to Celsius
         return kelvin - 273.15;
+    }
+
+    public static double convertK2F(double kelvin) { //Converts Kelvin to Fahrenheit
+        return convertC2F(convertK2C(kelvin));
     }
 
     public static double getTemp(String unit) {
@@ -37,8 +51,59 @@ public class TempConversion {
     }
 
     public static void main(String[] args) {
-        while(true) {
+        double result;
+        System.out.println("What would you like to convert from? ");
+        String in = getUnitChoice();
+        if (in.equals("Q")){
+            System.out.println("Goodbye");
+            exit(0);
+        }
+        System.out.println("What would you like to convert to? ");
+        String out = input.nextLine();
+        String convert = in + out;
+        if(out.equals("Q")){
+            System.out.println("Goodbye");
+            exit(0);
+        }
+        double inTemp = getTemp(in);
 
+        while(true){
+            switch (convert) {
+                case "CF" -> {
+                    result = convertC2F(inTemp);
+                    System.out.println("Conversion of " + inTemp + "°C is " + result + "°F");
+                    return;
+                }
+                case "CK" -> {
+                    result = convertC2K(inTemp);
+                    System.out.println("Conversion of " + inTemp + "°C is " + result + "°K");
+                    return;
+                }
+                case "FC" -> {
+                    result = convertF2C(inTemp);
+                    System.out.println("Conversion of " + inTemp + "°F is " + result + "°C");
+                    return;
+                }
+                case "FK" -> {
+                    result = convertF2K(inTemp);
+                    System.out.println("Conversion of " + inTemp + "°F is " + result + "°K");
+                    return;
+                }
+                case "KC" -> {
+                    result = convertK2C(inTemp);
+                    System.out.println("Conversion of " + inTemp + "°K is " + result + "°C");
+                    return;
+                }
+                case "KF" -> {
+                    result = convertK2F(inTemp);
+                    System.out.println("Conversion of " + inTemp + "°K is " + result + "°F");
+                    return;
+                }
+                default -> {
+                    System.out.println("Enter a valid unit for conversion");
+                    return;
+                }
+            }
         }
     }
 }
